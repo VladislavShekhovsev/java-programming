62a1143ab7015acfcf40ddfdf68a7051771a37ff
Vladislav <vladislav13457@gmail.com>
2021-05-18 10:32:24 -0400

    05/16/21 practiced some custom methods

---
+++ src/saim_HomeWork/practice_05_17_21/RectangleTest.java
@@ -0,0 +1,14 @@
+package saim_HomeWork.practice_05_17_21;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle.getArea());
+        rectangle.setDimensions(5, 3);
+        System.out.println(rectangle.getArea());
+        System.out.println(rectangle);
+
+        Rectangle rectangle2 = new Rectangle();
+        System.out.println(rectangle2.getArea());
+    }
+}
