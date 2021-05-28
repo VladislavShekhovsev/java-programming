42007250ef65d2d97f9fcf5b52ea24fa371b5b62
Vladislav <vladislav13457@gmail.com>
2021-05-27 22:08:11 -0400

    05/27/21 added student class for constructor

---
+++ src/day_47_constructors/Student.java
@@ -0,0 +1,19 @@
+package day_47_constructors;
+
+public class Student {
+    // no args constructor
+   public Student(){
+       System.out.println("no args constructor");
+   }
+   public Student(String name){
+       System.out.println("student name: "+name);
+   }
+   // constructor age
+    public Student(int age){
+        System.out.println("student age: "+age);
+    }
+    public Student(String name, int age){
+        System.out.println("Student name: "+name+", student age: "+age);
+    }
+
+}
